deeb5f9a54cafb2acfc89bd1746844359d3b1218
air-carbon <garoleg@karols-macbook-air.home>
2023-03-25 23:57:39 +0100

    TodoList.sol v1 working

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,5 +2,23 @@
 pragma solidity ^0.5.0;
 
 contract TodoList {
-    uint public taskCount = 0; //state variable
+    uint public taskCount = 0; // state variable
+
+    // task definition
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    // task list as a key-value pair, hash table (associative array)
+    mapping(uint => Task) public tasks; 
+
+    constructor() public {
+        createTask("First task for test");
+    }
+    function createTask(string memory _content) public { // accept one string argument
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
